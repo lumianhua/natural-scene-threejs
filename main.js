20ddb2f33d4b316bb87ffeae63b1254d259427c3
lumianhua <1459904836@qq.com>
2025-04-01 02:01:46 +0200

    boid test

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ import { RGBELoader } from 'https://esm.sh/three@0.154.0/examples/jsm/loaders/RG
 import { ImprovedNoise } from 'https://esm.sh/three@0.154.0/examples/jsm/math/ImprovedNoise.js';
 import { PMREMGenerator } from 'https://esm.sh/three@0.154.0';
 import { PointerLockControls } from 'https://esm.sh/three@0.154.0/examples/jsm/controls/PointerLockControls.js';
+import { GPUComputationRenderer } from 'https://esm.sh/three@0.154.0/examples/jsm/misc/GPUComputationRenderer.js';
+
+
 
 
 // preliminary
@@ -61,11 +64,240 @@ function wang(f, i, o = 0) {
 
 
 
+// boid
+
+const fragmentShaderPosition = `
+uniform float time;
+uniform float delta;
+
+void main() {
+  vec2 uv = gl_FragCoord.xy / resolution.xy;
+  vec4 tmpPos = texture2D( texturePosition, uv );
+  vec3 position = tmpPos.xyz;
+  vec3 velocity = texture2D( textureVelocity, uv ).xyz;
+
+  float phase = tmpPos.w;
+
+  phase = mod( 
+    phase + delta +
+    length( velocity.xz ) * delta * 3. +
+    max( velocity.y, 0.0 ) * delta * 6. , 
+    62.83 
+  );
+
+  gl_FragColor = vec4( position + velocity * delta * 15. , phase );
+}
+`;
+
+
+const fragmentShaderVelocity = `
+uniform float time;
+uniform float testing;
+uniform float delta;
+uniform float separationDistance;
+uniform float alignmentDistance;
+uniform float cohesionDistance;
+uniform float freedomFactor;
+uniform vec3 predator;
+
+const float width = resolution.x;
+const float height = resolution.y;
+
+const float PI = 3.141592653589793;
+const float PI_2 = PI * 2.0;
+
+float zoneRadius = 40.0;
+float zoneRadiusSquared = 1600.0;
+
+float separationThresh = 0.45;
+float alignmentThresh = 0.65;
+
+const float UPPER_BOUNDS = BOUNDS;
+const float LOWER_BOUNDS = -UPPER_BOUNDS;
+
+const float SPEED_LIMIT = 9.0;
+
+float rand( vec2 co ) {
+  return fract( sin( dot( co.xy, vec2(12.9898,78.233) ) ) * 43758.5453 );
+}
+
+void main() {
+  zoneRadius = separationDistance + alignmentDistance + cohesionDistance;
+  separationThresh = separationDistance / zoneRadius;
+  alignmentThresh = ( separationDistance + alignmentDistance ) / zoneRadius;
+  zoneRadiusSquared = zoneRadius * zoneRadius;
+
+  vec2 uv = gl_FragCoord.xy / resolution.xy;
+  vec3 birdPosition, birdVelocity;
+
+  vec3 selfPosition = texture2D( texturePosition, uv ).xyz;
+  vec3 selfVelocity = texture2D( textureVelocity, uv ).xyz;
+
+  float dist;
+  vec3 dir;
+  float distSquared;
+
+  float f;
+  float percent;
+
+  vec3 velocity = selfVelocity;
+  float limit = SPEED_LIMIT;
+
+  dir = predator * UPPER_BOUNDS - selfPosition;
+  dir.z = 0.0;
+  dist = length( dir );
+  distSquared = dist * dist;
+
+  float preyRadius = 150.0;
+  float preyRadiusSq = preyRadius * preyRadius;
+
+  if ( dist < preyRadius ) {
+    f = ( distSquared / preyRadiusSq - 1.0 ) * delta * 100.0;
+    velocity += normalize( dir ) * f;
+    limit += 5.0;
+  }
+
+  vec3 central = vec3( 0.0, 0.0, 0.0 );
+  dir = selfPosition - central;
+  dist = length( dir );
+  dir.y *= 2.5;
+  velocity -= normalize( dir ) * delta * 5.0;
+
+  for ( float y = 0.0; y < height; y++ ) {
+    for ( float x = 0.0; x < width; x++ ) {
+      vec2 ref = vec2( x + 0.5, y + 0.5 ) / resolution.xy;
+      birdPosition = texture2D( texturePosition, ref ).xyz;
+
+      dir = birdPosition - selfPosition;
+      dist = length( dir );
+      if ( dist < 0.0001 ) continue;
+
+      distSquared = dist * dist;
+      if ( distSquared > zoneRadiusSquared ) continue;
+
+      percent = distSquared / zoneRadiusSquared;
+
+      if ( percent < separationThresh ) {
+        f = ( separationThresh / percent - 1.0 ) * delta;
+        velocity -= normalize( dir ) * f;
+      } else if ( percent < alignmentThresh ) {
+        float threshDelta = alignmentThresh - separationThresh;
+        float adjustedPercent = ( percent - separationThresh ) / threshDelta;
+        birdVelocity = texture2D( textureVelocity, ref ).xyz;
+        f = ( 0.5 - cos( adjustedPercent * PI_2 ) * 0.5 + 0.5 ) * delta;
+        velocity += normalize( birdVelocity ) * f;
+      } else {
+        float threshDelta = 1.0 - alignmentThresh;
+        float adjustedPercent = threshDelta == 0.0 ? 1.0 : ( percent - alignmentThresh ) / threshDelta;
+        f = ( 0.5 - ( cos( adjustedPercent * PI_2 ) * -0.5 + 0.5 ) ) * delta;
+        velocity += normalize( dir ) * f;
+      }
+    }
+  }
+
+  if ( length( velocity ) > limit ) {
+    velocity = normalize( velocity ) * limit;
+  }
+
+  gl_FragColor = vec4( velocity, 1.0 );
+}
+`;
+
+
+
+
+
+
+const WIDTH = 8;
+const BOUNDS = 800;
+const BOUNDS_HALF = BOUNDS / 2;
+
+function fillPositionTexture(texture) {
+  const data = texture.image.data;
+  for (let i = 0; i < data.length; i += 4) {
+    const x = Math.random() * BOUNDS - BOUNDS_HALF;
+    const y = Math.random() * BOUNDS - BOUNDS_HALF;
+    const z = Math.random() * BOUNDS - BOUNDS_HALF;
+    data[i + 0] = x;
+    data[i + 1] = y;
+    data[i + 2] = z;
+    data[i + 3] = 1;
+  }
+}
+
+function fillVelocityTexture(texture) {
+  const data = texture.image.data;
+  for (let i = 0; i < data.length; i += 4) {
+    data[i + 0] = (Math.random() - 0.5) * 10;
+    data[i + 1] = (Math.random() - 0.5) * 10;
+    data[i + 2] = (Math.random() - 0.5) * 10;
+    data[i + 3] = 1;
+  }
+}
+
+
+
+
+
+
 
 
 
 // Create the scene
 const scene = new THREE.Scene();
+
+
+let gpuCompute;
+let velocityVariable, positionVariable;
+let positionUniforms, velocityUniforms;
+
+function initComputeRenderer(renderer) {
+  gpuCompute = new GPUComputationRenderer(WIDTH, WIDTH, renderer);
+
+  const dtPosition = gpuCompute.createTexture();
+  const dtVelocity = gpuCompute.createTexture();
+
+  fillPositionTexture(dtPosition);
+  fillVelocityTexture(dtVelocity);
+
+  positionVariable = gpuCompute.addVariable("texturePosition", fragmentShaderPosition, dtPosition);
+  velocityVariable = gpuCompute.addVariable("textureVelocity", fragmentShaderVelocity, dtVelocity);
+
+  gpuCompute.setVariableDependencies(positionVariable, [positionVariable, velocityVariable]);
+  gpuCompute.setVariableDependencies(velocityVariable, [positionVariable, velocityVariable]);
+
+  positionUniforms = positionVariable.material.uniforms;
+  velocityUniforms = velocityVariable.material.uniforms;
+
+  positionUniforms.time = { value: 0.0 };
+  positionUniforms.delta = { value: 0.0 };
+
+  velocityUniforms.time = { value: 0.0 };
+  velocityUniforms.delta = { value: 0.0 };
+  velocityUniforms.separationDistance = { value: 20.0 };
+  velocityUniforms.alignmentDistance = { value: 40.0 };
+  velocityUniforms.cohesionDistance = { value: 40.0 };
+  velocityUniforms.freedomFactor = { value: 0.75 };
+  velocityUniforms.predator = { value: new THREE.Vector3() };
+
+  velocityVariable.material.defines.BOUNDS = BOUNDS.toFixed(2);
+
+  const error = gpuCompute.init();
+  if (error) console.error(error);
+}
+
+const antelopeGeometry = new THREE.BoxGeometry(2, 1, 5); // 比例像个动物身体
+
+
+const antelopeMaterial = new THREE.MeshStandardMaterial({ color: 0x886633 });
+const antelopeMesh = new THREE.InstancedMesh(antelopeGeometry, antelopeMaterial, WIDTH * WIDTH);
+antelopeMesh.castShadow = true;
+antelopeMesh.receiveShadow = true;
+scene.add(antelopeMesh);
+
+const dummy = new THREE.Object3D();
+
+
 
 
 // Create the camera
@@ -80,6 +312,12 @@ camera.position.set(0, 0, 0);
 // Create the renderer
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap; // Soften the shadows
+
+initComputeRenderer(renderer);
+
+
 document.body.appendChild(renderer.domElement);
 
 
@@ -143,9 +381,28 @@ rgbeLoader.load('assets/hdr/industrial_sunset_02_puresky_4k.hdr', (hdrEquirect) 
   // Create and add direct light (to simulate sunlight)
   const light = new THREE.DirectionalLight(0xffffff, 1.2);
   light.position.copy(sunDirection.clone().multiplyScalar(100));
+  light.castShadow = true;
+  
+  light.shadow.mapSize.width = 2048;
+  light.shadow.mapSize.height = 2048;
+
+  light.shadow.camera.left = -500;
+  light.shadow.camera.right = 500;
+  light.shadow.camera.top = 500;
+  light.shadow.camera.bottom = -500;
+  light.shadow.camera.near = 1;
+  light.shadow.camera.far = 1000;
+
   scene.add(light);
 
-  // Add debugging of the sun's position to the blob
+  
+  // add helper to debug
+
+  // // Debugging light source shadow area
+  // const helper = new THREE.CameraHelper(light.shadow.camera);
+  // scene.add(helper);
+
+  
   const sunHelper = new THREE.Mesh(
     new THREE.SphereGeometry(5, 16, 16),
     new THREE.MeshBasicMaterial({ color: 0xffff00 })
@@ -206,12 +463,6 @@ document.addEventListener('keyup', (event) => {
     case 'KeyE': move.down = false; break;
   }
 });
-
-
-
-
-  
-
 
 
 
@@ -407,6 +658,8 @@ generateWangTextures((textures) => {
   geometry.attributes.uv.needsUpdate = true;
 
   const terrain = new THREE.Mesh(geometry, material);
+  terrain.castShadow = true;
+  terrain.receiveShadow = true;
   scene.add(terrain);
 });
 
@@ -430,6 +683,21 @@ generateWangTextures((textures) => {
 // Animation
 function animate() {
     requestAnimationFrame(animate);
+    const now = performance.now();
+    let delta = (now - (animate.lastTime || now)) / 1000;
+    animate.lastTime = now;
+
+    positionUniforms.time.value = now;
+    positionUniforms.delta.value = delta;
+    velocityUniforms.time.value = now;
+    velocityUniforms.delta.value = delta;
+
+    
+    
+    gpuCompute.compute();
+
+
+
     const speed = 1.5;
 
     direction.z = Number(move.forward) - Number(move.backward);
@@ -446,6 +714,34 @@ function animate() {
     controls.getObject().position.y += velocity.y;
     if (sky) sky.position.copy(camera.position);
 
+    
+    const target = gpuCompute.getCurrentRenderTarget(positionVariable);
+    const posTexture = target.texture;
+    const readPixels = new Float32Array(WIDTH * WIDTH * 4);
+    
+    renderer.readRenderTargetPixels(
+      target,
+      0, 0, WIDTH, WIDTH,
+      readPixels
+    );
+    
+
+    for (let i = 0; i < WIDTH * WIDTH; i++) {
+      const x = readPixels[i * 4];
+      const y = readPixels[i * 4 + 1];
+      const z = readPixels[i * 4 + 2];
+
+      dummy.position.set(x, 5, z);
+
+
+
+      dummy.updateMatrix();
+      antelopeMesh.setMatrixAt(i, dummy.matrix);
+    }
+    antelopeMesh.instanceMatrix.needsUpdate = true;
+
+
+
 
   
 
@@ -454,3 +750,9 @@ function animate() {
   
   animate();
   
+
+
+
+
+
+
